bebafe5224b8c393ad1fe687a1885876e373a959
Anders Mannberg <anders.mannberg@softhouse.se>
2020-04-25 19:44:41 +0200

    Initial Commit

---
+++ TDD-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDD-Swift
+//
+//  Created by Anders Mannberg on 2020-04-25.
+//  Copyright © 2020 Mannberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TDD-SwiftTests/TDD_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  TDD_SwiftTests.swift
+//  TDD-SwiftTests
+//
+//  Created by Anders Mannberg on 2020-04-25.
+//  Copyright © 2020 Mannberg. All rights reserved.
+//
+
+import XCTest
+@testable import TDD_Swift
+
+class TDD_SwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
